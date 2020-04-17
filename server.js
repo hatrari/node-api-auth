88ae83f971dbf2708695a860960c9a63564c49fb
Atrari Hamza <hatrari@L530342.SQLI>
2020-04-17 11:10:47 +0200

    creating routes folder and subcribres file

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ db.once('open', () => console.log('connected to database'))
 
 app.use(express.json())
 
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
 app.listen(3000, () => console.log('server started'))
