e6d6142c72b062c3310462afbf3e83481f5cc122
Atrari Hamza <hatrari@L530342.SQLI>
2020-04-17 15:23:20 +0200

    setting up book controller

---
+++ controllers/book.js
@@ -0,0 +1,33 @@
+const Book = require('../models/book');
+
+exports.create = (req, res) => {
+  const book = new Book(req.body.book);
+  book.save()
+  .then(response => res.status(201).json(response))
+  .catch(error => res.status(400).json(error));
+};
+
+exports.get = (req, res) => {
+  Book.findOne({_id: req.params.id})
+  .then(response => res.status(200).json(response))
+  .catch(error => res.status(404).json(error));
+};
+
+exports.update = (req, res) => {
+  const book = new Book(req.body.book);
+  Book.updateOne({_id: req.params.id}, book)
+  .then(response => res.status(201).json(response))
+  .catch(error => res.status(400).json(error));
+};
+
+exports.delete = (req, res) => {
+  Book.deleteOne({_id: req.params.id})
+  .then(response => res.status(200).json(response))
+  .catch(error => res.status(400).json(error));
+};
+
+exports.all = (req, res) => {
+  Book.find()
+  .then(response => res.status(200).json(response))
+  .catch(error => res.status(400).json(error));
+};
